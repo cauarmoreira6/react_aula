daa83a6a0bfc639a0e1578f7f5a54fb4ae7f9aa4
Cauã Ramalho Moreira <caua.r.moreira6@aluno.senai.br>
2026-04-22 16:38:10 -0300

    dia 22/04 finalizado

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './componentes/Header';
+import styled from 'styled-components';
+
+const AppContainer = `
+  width: 100vw;
+  height: 100vw;
+  background - image: linear - gradient(90deg, #002f52, #326589);
+
+
+li {
+  list - style: none;
+}
+`
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+    </AppContainer>
+  );
+}
+
+export default App;
